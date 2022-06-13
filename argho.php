a7663d83208c090b6a3b487c0d8bc649e6fb473c
ArghoProshad <singho.argho.42906@gmail.com>
2022-06-13 20:28:43 +0600

    done

---
+++ argho.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>create a new account</title>
+</head>
+<body>
+		<form action="check.php" method="post" enctype="">
+			<fieldset>
+				<legend>create a new account</legend>
+				<table>
+					<tr>
+						<td>Name</td>
+						<td><input type="text" name="name" value=""></td>
+					</tr>
+					<tr>
+						<td>National id</td>
+						<td><input type="text" name="id" value=""></td>
+						<td>Email</td>
+						<td><input type="email" name="email" value=""></td>
+						<td>DOB</td>
+						<td><input type="date" name="date" value=""></td>
+						<td>password</td>
+						<td><input type="password" name="password" value=""></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" name="" value="Register"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+</body>
+</html> 
