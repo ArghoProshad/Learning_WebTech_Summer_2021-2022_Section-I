90ce36f5f2d70dd4f62b503bfed00fbfc5121a36
ArghoProshad <102038212+ArghoProshad@users.noreply.github.com>
2022-06-20 14:42:32 +0600

    done

---
+++ Home.php
@@ -0,0 +1,15 @@
+  <html>
+
+<body>
+
+
+<h1>Welcome Anne!</h1>
+<p><a href ="Registration.php">Profile</p>
+<p><a href ="Registration.php">Change_Password</p>
+<p><a href ="Registration.php">Logout</p>
+
+</body>
+
+
+</html>
+
---
+++ Registration.php
@@ -0,0 +1,51 @@
+
+<html>
+<head>
+	<title>registration Form Submission</title>
+</head>
+<body>
+		<form action="loginREGCheck.php" method="post" enctype="">
+			<fieldset>
+				<legend>Registration</legend>
+				<table>
+					<tr>
+						<td>ID</td>
+						</tr>
+						<tr>
+						<td><input type="text" name="id" value=""></td>
+					</tr>
+					<tr>
+						<td>password</td></tr>
+						<tr>
+						<td><input type="password" name="password" value=""></td>
+					</tr>
+					<tr>
+						<td>confirm_ password</td></tr>
+						<tr>
+						<td><input type="password" name="password" value=""></td>
+					</tr>
+					<tr>
+						<td>Name</td></tr>
+						<tr>
+						<td><input type="text" name="username" value=""></td>
+					</tr>
+					<tr>
+						<td>user_ type</td></tr>
+						<tr>
+						<td><hr></td>
+						</tr>
+						<tr>
+						<td><input type="radio" name="user" value="user">USER<input type="radio" name="user" value="user">admin</td>
+						
+					</tr>
+					<tr>
+						<td>
+							<input type="submit" name="" value="Submit">
+							<a href="signup.html"> Resisitration </a>
+						</td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+</body>
+</html>
